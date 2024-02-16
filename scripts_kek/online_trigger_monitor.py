cefb25a6a62c2568d22e874a4425161bd1fc1533
Sungwoon <suchoi@cern.ch>
2024-02-16 19:45:03 +0900

    Developing online trigger monitoing, almost done for prototype

--- scripts_kek/online_trigger_monitor.py
+++ scripts_kek/online_trigger_monitor.py
@@ -25,28 +25,31 @@ if __name__ == "__main__":
 
     dt = args.dt
 
-    start = datetime.datetime.now()
+    start = datetime.datetime.now().replace(microsecond=0)
+    readtime = list()
 
-    lines = content.split("\n")
     data = list()
     isnew = False
     data_for_time = list()
     times = list()
 
+    past = start
 
     while(True):
         now = datetime.datetime.now().replace(microsecond=0)
-        dt_now = now-start
+        dt_now = now-past
         dt_now = int(dt_now.total_seconds())
         if  dt_now > dt:
-            start = datetime.datetime.now()
+            myreadtime = now-start
+            readtime.append(int(myreadtime.total_seconds()))
+            past = now
         else:
             continue
 
         with open(os.path.join(sw_path,datafile),"r") as file:
             content = file.read()
-            content=content.strip("\n")
-
+            content = content.strip("\n")
+            lines = content.split("\n")
 
         for line in lines:
             if line[0] == '$':
@@ -54,7 +57,7 @@ if __name__ == "__main__":
             if line[0] == '#':
                 isnew = True
                 mytime = int(line[1:])
-                #times.append(mytime)
+                times.append(mytime)
                 continue
             else :
                 data_for_time.append(line)
@@ -63,6 +66,41 @@ if __name__ == "__main__":
                 data_for_time = list()
                 isnew = False
 
+        data = sum(data, [])               
+        filtered_data = [item for item in data if item.startswith('b')]
+        modified_data = [item[1:] for item in filtered_data]
+        data_trg_c = [item for item in modified_data if item.endswith(' &')]
+        data_trg_s = [item for item in modified_data if item.endswith(' %')]
+        trg_c = [item[:-2] for item in data_trg_c]
+        trg_s = [item[:-2] for item in data_trg_s]
+       
+        trg_c = [item.zfill(4) for item in trg_c]
+        trg_s = [item.zfill(4) for item in trg_s]
+ 
+        trg_c_ch3 = sum(1 for item in trg_c if '1' in str(item) and str(item)[0] == '1')
+        trg_c_ch2 = sum(1 for item in trg_c if '1' in str(item) and str(item)[1] == '1')
+        trg_c_ch1 = sum(1 for item in trg_c if '1' in str(item) and str(item)[2] == '1')
+        trg_c_ch0 = sum(1 for item in trg_c if '1' in str(item) and str(item)[3] == '1')
+
+        trg_s_ch3 = sum(1 for item in trg_s if '1' in str(item) and str(item)[0] == '1')
+        trg_s_ch2 = sum(1 for item in trg_s if '1' in str(item) and str(item)[1] == '1')
+        trg_s_ch1 = sum(1 for item in trg_s if '1' in str(item) and str(item)[2] == '1')
+        trg_s_ch0 = sum(1 for item in trg_s if '1' in str(item) and str(item)[3] == '1')
+
+#        trg_c_ch3 = [item for item in trg_c if '1' in str(item) and str(item)[0] == '1']
+#        trg_c_ch2 = [item for item in trg_c if '1' in str(item) and str(item)[1] == '1']
+#        trg_c_ch1 = [item for item in trg_c if '1' in str(item) and str(item)[2] == '1']
+#        trg_c_ch0 = [item for item in trg_c if '1' in str(item) and str(item)[3] == '1']
+
+#        trg_s_ch3 = [item for item in trg_s if '1' in str(item) and str(item)[0] == '1']
+#        trg_s_ch2 = [item for item in trg_s if '1' in str(item) and str(item)[1] == '1']
+#        trg_s_ch1 = [item for item in trg_s if '1' in str(item) and str(item)[2] == '1']
+#        trg_s_ch0 = [item for item in trg_s if '1' in str(item) and str(item)[3] == '1']
+
+        print(trg_c_ch3)
+        print("len(data): {}".format(len(data)))
+        print("--------------------------------------------------------------")
+        data = list()
 
     print(len(times))
     print("===================================")
