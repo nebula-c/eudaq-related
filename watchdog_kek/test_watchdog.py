6c1b1ab96a93a60f59d05f56c5e723da29aac282
Sungwoon <suchoi@cern.ch>
2024-03-09 18:32:56 +0900

    Changed hook url

--- watchdog_kek/test_watchdog.py
+++ watchdog_kek/test_watchdog.py
@@ -24,8 +24,8 @@ def send_to_mm(msg, hook_url):
     # print(r.text)
     return r.text
 
-def isBeamTrigger(interval,mycondition):
-    numTrig = getScintTrig(dt=interval,condition_input=condition(mycondition))
+def isBeamTrigger(triggerdt,mycondition):
+    numTrig = getScintTrig(dt=triggerdt,condition_input=condition(mycondition))
     return numTrig
     # print(f"numTrig: {numTrig}")
     # if numTrig > 3000:
@@ -41,72 +41,85 @@ def isEUDAQOn():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Watch the status of datataking and inform on Mattermost.")
     # parser.add_arg    ument("channel", help="Mattermost channel to write to.")
-    parser.add_argument("--interval", "-i", default=3, type=float, help="Monitoring interval in sec (default 30).")
+    parser.add_argument("--interval", "-i", default=20, type=float, help="Monitoring interval in min (default 20).")
+    parser.add_argument("--triggerdt", default=5, type=float, help="dt for checking trigger board in sec(default 5).")
 #    parser.add_argument("-t", "--test", action="store_true", help="Test connection to Mattermost and exit.")
-    parser.add_argument("--hook", default="https://mattermost.web.cern.ch/hooks/uup6cn6xyinuid9n9z74psc14o", help="Mattermost webhook URL")
+    parser.add_argument("--hook", default="https://mattermost.web.cern.ch/hooks/383ozy9s7b8e583h4t1dm5jrca", help="Mattermost webhook URL")
     parser.add_argument("--trig", action="store_true", help="Check trigger status")
     parser.add_argument("--logic", "-l", default="RXXX", help="Set trigger board logic to monitoring")
 #    parser.add_argument("--scopeOn", action="store_true", help="Send scope ready for trigger when no trigger 10 min")
     args = parser.parse_args()
 
-    interval = args.interval
+    interval = args.interval * 60
+    triggerdt = args.triggerdt
     hook = args.hook
     mylogic = args.logic
     start = datetime.now().replace(microsecond=0)
+    isFirst = True
 
     while (True):
         with open("log_watchdog.log","a") as file: 
             try:
                 now = datetime.now().replace(microsecond=0)
-                # dt = now-start
-                # dt = int(dt.total_seconds())
+                dt = now-start
+                dt = int(dt.total_seconds())
 
-                beamrate = isBeamTrigger(interval,mylogic)
-                # print(beamrate)
-                if isEUDAQOn() == True:
-                    EUDAQ_status = "ON"
-                else:
-                    EUDAQ_status = "OFF"
-                msg = """
+#                 beamrate = isBeamTrigger(dt,mylogic)
+#                 # print(beamrate)
+#                 if isEUDAQOn() == True:
+#                     EUDAQ_status = "ON"
+#                 else:
+#                     EUDAQ_status = "OFF"
+#                 msg = """
+#             =================================================
+#                 System time : {}
+#                 Message from OSM(DAQ PC)
+#                 Beamrate : {:.2f} Hz
+#                 EUDAQ : {}
+#             =================================================
+# "Beamrate" in here is assumed valued from trigger board and based on scintillator.
+# "EUDAQ" status means if EUDAQ operation window is opening. It couldn't mean EUDAQ is fine                 
+#                 """.format(now,beamrate/interval,EUDAQ_status)
+                
+#                 myres = send_to_mm(msg,hook)
+#                 mylog = "{} | Monitoring status : {}\n".format(now,myres)
+#                 file.write(mylog)
+#                 file.close()
+
+                if dt % interval == 0 or isFirst == True:
+                    isFirst = False
+                    beamrate = isBeamTrigger(triggerdt,mylogic)
+                    
+                    # print(beamrate)
+                    if isEUDAQOn() == True:
+                        EUDAQ_status = "ON"
+                        
+                    else:
+                        EUDAQ_status = "OFF"
+
+
+                    msg = """
             =================================================
+                        !!! Just test message !!!
                 System time : {}
                 Message from OSM(DAQ PC)
                 Beamrate : {:.2f} Hz
                 EUDAQ : {}
             =================================================
+It is developing now.... So it is just test message and beamrate is pulse of function generator.
 "Beamrate" in here is assumed valued from trigger board and based on scintillator.
 "EUDAQ" status means if EUDAQ operation window is opening. It couldn't mean EUDAQ is fine                 
-                """.format(now,beamrate/interval,EUDAQ_status)
-                
-                myres = send_to_mm(msg,hook)
-                mylog = "{} | Monitoring status : {}\n".format(now,myres)
-                file.write(mylog)
-                file.close()
-
-                # if dt % interval == 0:
-                #     beamrate = isBeamTrigger(mylogic)
-                #     # print(beamrate)
-                #     if isEUDAQOn() == True:
-                #         EUDAQ_status = "ON"
-                #     else:
-                #         EUDAQ_status = "OFF"
-                #     msg = """
-                #     =======================================
-                #     System time : {}
-                #     This is test text from OSM(DAQ PC)
-                #     Beamrate : {:.2f} Hz
-                #     EUDAQ : {}
-                #     =======================================
-                #     """.format(now,beamrate,EUDAQ_status)
+                    """.format(now,beamrate/triggerdt,EUDAQ_status)
                     
 
-                #     myres = send_to_mm(msg,hook)
+
+                    myres = send_to_mm(msg,hook)
 
 
-                #     mylog = "{} | Monitoring status : {}\n".format(now,myres)
-                #     file.write(mylog)
-                #     file.close()
-                #     time.sleep(interval*0.6)
+                    mylog = "{} | Monitoring status : {}\n".format(now,myres)
+                    file.write(mylog)
+                    file.close()
+                    time.sleep(interval*0.8)
                     
             except Exception as e:
                 mylog = "{} | MONITORING ERROR : {}".format(now,e)
